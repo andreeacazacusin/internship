7e6843a4d4ebb337cdea92aac91fb49bfddf8c8a
WIRTEK\andreea.cazacu <andreea.cazacu@wirtek.com>
2017-08-21 16:04:23 +0300

    Delete book

--- TestFormsApp/TestFormsApp/BooksForm.Designer.cs
+++ TestFormsApp/TestFormsApp/BooksForm.Designer.cs
@@ -28,22 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.booksGridView = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // booksGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(24, 28);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(652, 221);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.booksGridView.AllowUserToAddRows = false;
+            this.booksGridView.AllowUserToDeleteRows = false;
+            this.booksGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.booksGridView.Location = new System.Drawing.Point(24, 28);
+            this.booksGridView.Name = "booksGridView";
+            this.booksGridView.ReadOnly = true;
+            this.booksGridView.Size = new System.Drawing.Size(652, 221);
+            this.booksGridView.TabIndex = 0;
+            this.booksGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
@@ -61,18 +61,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 261);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.booksGridView);
             this.Name = "BooksForm";
             this.Text = "Books";
             this.Load += new System.EventHandler(this.Books_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView booksGridView;
         private System.Windows.Forms.Button button1;
     }
 }
--- TestFormsApp/TestFormsApp/BooksForm.cs
+++ TestFormsApp/TestFormsApp/BooksForm.cs
@@ -33,13 +33,47 @@ namespace TestFormsApp
                     Author = x.Author,
                     Description = x.Description
                 }).ToList();
-                this.dataGridView1.DataSource = books;
+                DataGridViewButtonColumn buttonColEdit = new DataGridViewButtonColumn();
+                buttonColEdit.Name = "Edit";
+                buttonColEdit.Text = "Edit";
+                buttonColEdit.UseColumnTextForButtonValue = true;
+                DataGridViewButtonColumn buttonColDelete = new DataGridViewButtonColumn();
+                buttonColDelete.Name = "Delete";
+                buttonColDelete.Text = "Delete";
+                buttonColDelete.UseColumnTextForButtonValue = true;
+
+                booksGridView.Columns.Add(buttonColEdit);
+                booksGridView.Columns.Add(buttonColDelete);
+                this.booksGridView.DataSource = books;
             }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            Console.WriteLine(sender);
+            if (e.ColumnIndex == 0)
+            {
+                MessageBox.Show("EDIT button clicked at row: " + this.booksGridView.Rows[e.RowIndex].Cells[2].Value);
+                
+            }
+            else
+            {
+                if (e.ColumnIndex == 1)
+                {
+                    DeleteBook((int)booksGridView.Rows[e.RowIndex].Cells["Id"].Value);
+                    RefreshData();
+                    MessageBox.Show("Book deleted!");
+                }                
+            }
+        }
+
+        private void DeleteBook(int bookId)
+        {
+            using (var context = new LibraryEntities())
+            {
+                var book = context.Books.FirstOrDefault(b => b.Id == bookId);
+                context.Books.Remove(book);
+                context.SaveChanges();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
